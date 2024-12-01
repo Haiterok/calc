c6405132889910ba241fc659c5aa394fa7142396
Haiterok <haiterok7@gmail.com>
2024-12-01 14:33:00 +0200

    Added memory

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const screen = document.getElementById("screen")
+const memoryDisplay = document.getElementById("memoryDisplay")
+
+let memory = 0;
 
 const addToScreen = (input) => {
     if(screen.value === '0' || screen.value === 'Error') {
         screen.value = ''
     }
-    screen.value += input
-}
 
-const addDot = () => {
-    if (!screen.value.includes('.')) {
-        screen.value += '.'
+    if (input === '.'){
+        if (!screen.value.includes('.')) {
+            screen.value += '.'
+        }
+    }else{
+        screen.value += input
     }
 }
 
@@ -30,4 +34,29 @@ const count = () => {
     } catch (err) {
         screen.value = 'Error'
     }
+}
+
+const changeMemory = (input) => {
+    if(input === '+'){
+        memory +=Number(eval(screen.value))
+        memoryDisplay.innerText = memory
+    }else if(input === '-'){
+        memory -= Number(eval(screen.value))
+        memoryDisplay.innerText = memory
+    }
+
+    if (memory > 0 || memory < 0) {
+        memoryDisplay.style.display = 'block'
+    }else{
+        memoryDisplay.style.display = 'none'
+    }
+}
+
+const clearMemory = () => {
+    memory = 0;
+    memoryDisplay.style.display = 'none'
+}
+
+const putMemoryOnScreen = () => {
+    screen.value += memory;
 }
